e437a18da88e16302919268bc243f5528267e82e
S10243303@connect.np.edu.sg <S10243303@connect.np.edu.sg>
2024-01-05 07:17:41 +0000

    abc

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,3 +1,4 @@
+-- {{ config (materialized='view')}}
 select
 --from raw_orders
 o.orderid,
@@ -16,6 +17,7 @@ country,
 category,
 productname,
 subcategory,
+{{ markup() }} as markup, 
 ordersellingprice - ordercostprice as orderprofit
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
